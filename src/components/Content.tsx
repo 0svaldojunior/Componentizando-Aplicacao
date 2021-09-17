eefbce4b8df7acd86d997ea72611bd0505aa0376
0svaldojunior <osvaldo.rodrigues@acad.pucrs.br>
2021-09-17 20:39:20 -0300

    Desafio finalizado

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,35 @@
-export function Content() {
+import { MovieCard } from "./MovieCard";
+
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+interface ContentProps {
+  movies: MovieProps[];
+}
+
+export function Content(props: ContentProps) {
   // Complete aqui
+  return(
+    <div className="movies-list">
+    {
+      props.movies.map(movie => (
+        <MovieCard 
+          key ={movie.imdbID}
+          title={movie.Title}
+          poster={movie.Poster}
+          runtime={movie.Runtime}
+          rating={movie.Ratings[0].Value}
+        />
+      ))
+    }
+  </div>
+  );
 }
